03037aafd12345ebdf2ac908dd4b63835c5e80c7
sckimynwa <tsb03672@naver.com>
2021-11-13 16:02:39 +0900

    feat: add some logics

--- src/components/ObjectDetector/index.tsx
+++ src/components/ObjectDetector/index.tsx
@@ -1,4 +1,13 @@
-import React, { FC, MutableRefObject, useEffect, useRef } from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as tf from "@tensorflow/tfjs";
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import React, {
+  FC,
+  MutableRefObject,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import styled from "styled-components";
 
 import { theme } from "@/constants/theme";
@@ -7,39 +16,60 @@ const Wrapper = styled.div`
   padding: 32px;
 `;
 
+const ResultWrapper = styled.div`
+  margin-top: 50px;
+`;
+
+const ResultItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ResultLabel = styled.div``;
+
+const ResultProbability = styled.div``;
+
 const StreamingVideo = styled.video`
+  width: 646px;
+  height: 486px;
   border: 3px solid ${theme.colors.gray30};
   border-radius: 20px;
 `;
 
+const Button = styled.button``;
+
 const ObjectDetector: FC = () => {
+  const [modelLoading, setModelLoading] = useState<boolean>(false);
+  const [predictions, setPredictions] = useState([]);
+  const [webcam, setWebcam] = useState<any>();
+  const [model, setModel] = useState<mobilenet.MobileNet>();
   const videoRef = useRef<HTMLVideoElement>();
 
   const webcamLaunch = async () => {
+    setModelLoading(() => true);
     const stream = await navigator.mediaDevices.getUserMedia({ video: true });
     if (videoRef.current) {
       videoRef.current.srcObject = stream;
     }
+    const webcamIterator = await tf.data.webcam(videoRef.current);
+    setWebcam(webcamIterator);
 
-    // const webcamIterator = await tf.data.webcam(videoRef.current);
-    // const img = await webcamIterator.capture();
-    // img.print();
+    // mobile net load
+    const model = await mobilenet.load();
+    setModel(model);
+    setModelLoading(() => false);
+  };
+
+  const onPredict = async () => {
+    const img = await webcam.capture();
+    const result = await model?.classify(img);
+    setPredictions(result as any);
+    console.log(result);
   };
 
   useEffect(() => {
     webcamLaunch();
   }, []);
-
-  // const getUserMediaSupported = () => {
-  //   return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
-  // };
-
-  // const onClick = () => {
-  //   if (getUserMediaSupported()) {
-  //   } else {
-  //     console.warn("webcam is not supported in your browser");
-  //   }
-  // };
 
   return (
     <Wrapper>
@@ -47,6 +77,17 @@ const ObjectDetector: FC = () => {
         ref={videoRef as MutableRefObject<HTMLVideoElement>}
         autoPlay
       />
+      <Button onClick={onPredict} disabled={modelLoading}>
+        Predict
+      </Button>
+      <ResultWrapper>
+        {predictions.map((prediction: any) => (
+          <ResultItem>
+            <ResultLabel>{prediction.className}</ResultLabel>
+            <ResultProbability>{prediction.probability}</ResultProbability>
+          </ResultItem>
+        ))}
+      </ResultWrapper>
     </Wrapper>
   );
 };
